f1d219feef1f402b3ecd34f64f7b400af96b0ae8
thachnguyen181 <nguyenthachtubd@gmail.com>
2019-07-04 18:48:27 +0700

    FirstCommit-JPAtest

---
+++ src/main/java/tma/com/repository/Sales.java
@@ -0,0 +1,18 @@
+package tma.com.repository;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Sales")
+public class Sales implements Serializable {
+	
+	
+}
